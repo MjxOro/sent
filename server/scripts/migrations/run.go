8fc1d1560e241d15f1adae3eed4b9ff42a5c495e
mjx <mattheweoro@gmail.com>
2025-04-12 17:52:00 -0400

    fix: Change env variable name for db conection.

--- server/scripts/migrations/run.go
+++ server/scripts/migrations/run.go
@@ -21,12 +21,12 @@ func main() {
 
 	// Connect to database
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_SSLMODE"),
 	)
 
 	db, err := sql.Open("pgx", connStr)
