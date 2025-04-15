d7af169ab36f3899e89bbd3021ae29646c6edfc1
mjx <mattheweoro@gmail.com>
2025-04-15 02:08:10 -0400

    feat: Wrap root page with authproviders and change component structure.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,15 @@
 "use client";
-import { ExitStoreProvider } from "@/providers/store-provider";
-import dynamic from "next/dynamic";
-// Dynamic imports for better code splitting
-const LandingPageCanvas = dynamic(
-  () => import("@/components/LandingPage/LandingPageCanvas"),
-  {
-    ssr: false,
-  },
-);
-import LandingDOM from "@/components/LandingPage/LandingPageDOM";
 
-export default function Home() {
+import { AuthProvider } from "@/providers/auth-provider";
+import { ExitStoreProvider } from "@/providers/store-provider";
+import LandingContent from "@/components/LandingPage/LandingContent";
+
+export default function Page() {
   return (
-    <ExitStoreProvider>
-      <LandingPageCanvas />
-      <LandingDOM />
-    </ExitStoreProvider>
+    <AuthProvider>
+      <ExitStoreProvider>
+        <LandingContent />
+      </ExitStoreProvider>
+    </AuthProvider>
   );
 }
