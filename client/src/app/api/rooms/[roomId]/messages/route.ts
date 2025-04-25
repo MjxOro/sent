d8ebdb744cce291e7776485d6494c4c6aab12ea2
mjx <mattheweoro@gmail.com>
2025-04-24 23:06:57 -0400

    feat: Create nexjs server route to get messages from backend api.

---
+++ client/src/app/api/rooms/[roomId]/messages/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: { roomId: string } },
+) {
+  try {
+    const { params } = context;
+    const { roomId } = params;
+    const offset = request.nextUrl.searchParams.get("offset") || "0";
+    const limit = request.nextUrl.searchParams.get("limit") || "20";
+    const token = (await cookies()).get("auth_token")?.value;
+    // Forward the request to your Go backend
+    const response = await fetch(
+      `${process.env.SERVERURI || "http://localhost:8080"}/api/rooms/${roomId}/messages?offset=${offset}&limit=${limit}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+
+    // Return the response from your Go backend
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to get rooms" }, { status: 500 });
+  }
+}
