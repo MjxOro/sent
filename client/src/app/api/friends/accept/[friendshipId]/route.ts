24b16c4a9d341a71c0056e04ddbbeac1f383925d
mjx <mattheweoro@gmail.com>
2025-04-30 22:05:18 -0400

    feat: Create accept and reject friend request server routes.

---
+++ client/src/app/api/friends/accept/[friendshipId]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(
+  _: NextResponse,
+  { params }: { params: { friendshipId: string } },
+) {
+  try {
+    const { friendshipId } = params;
+    const token = (await cookies()).get("auth_token")?.value;
+    // Forward the request to your Go backend
+    const response = await fetch(
+      `${process.env.SERVERURI || "http://localhost:8080"}/api/friends/accept/${friendshipId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+    console.log(data);
+    // Return the response from your Go backend
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to accept friend request" },
+      { status: 500 },
+    );
+  }
+}
