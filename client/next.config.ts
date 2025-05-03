7ebb02f913997d5ef25e7bb5ae27b0bfdddb5fda
mjx <mattheweoro@gmail.com>
2025-05-03 13:05:26 -0400

    feat: Create remote exceptions for external images.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Or your other configurations
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.ggpht.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ggpht.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
